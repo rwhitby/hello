40ab9bdbd57b75c79a84ef9b179c887c97fa78aa
Rod Whitby <rod@whitby.id.au>
2011-06-13 16:35:13 +0930

    Simplified the layout

--- enyo/hello.js
+++ enyo/hello.js
@@ -5,19 +5,13 @@ enyo.kind(
     className: 'enyo-fit enyo-vflexbox main',
     components: [
 
-	{kind: 'SlidingPane', flex: 1, wideWidth: 800, components: [
-	     {name: 'list', width: '320px', components: [
-		  {kind: wi.Header, random: [
-		       {weight: 1,  tagline: 'You Say Goodbye, But I Say Hello!'}
-		   ]},
-		  
-		  {name: 'controls', components: [
-		       {name: 'htmlElement', kind: 'Item', content: 'Hello Enyo!'},
-		       {name: 'csrvElement', kind: 'Item', content: 'Waiting for C Service ...'},
-		       {name: 'nodeElement', kind: 'Item', content: 'Waiting for Node Service ...'}
-		   ]}
-	      ]}
+	{kind: wi.Header, random: [
+	     {weight: 1,  tagline: 'You Say Goodbye, But I Say Hello!'}
 	 ]},
+	
+	{name: 'enyoElement', kind: 'Item', content: 'Hello Enyo!'},
+	{name: 'csrvElement', kind: 'Item', content: 'Waiting for C Service ...'},
+	{name: 'nodeElement', kind: 'Item', content: 'Waiting for Node Service ...'},
 		
 	{name: 'cservice', kind: 'PalmService',
 	 service: 'palm://org.webosinternals.hello.c/', method: 'hello',
